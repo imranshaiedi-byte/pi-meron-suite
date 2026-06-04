81d5e44ed86d643525f0cf4d6f8faba6b7e7992e
imranshaiedi-byte <imranshaiedi@gmail.com>
2026-06-04 20:43:56 +0800

    revert: remove turn separators and widgets, restore original footer

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,6 @@ import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
 import { registerToolDisplayOverrides } from "./tool-overrides.js";
 import { registerThinkingLabeling } from "./thinking-label.js";
 import { DEFAULT_TOOL_DISPLAY_CONFIG, type ToolDisplayConfig } from "./types.js";
-import { registerSessionTracker } from "./session-tracker.js";
-import { registerWidgets, injectTurnSeparator } from "./widgets.js";
-import { registerTurnSeparatorRenderer } from "./turn-separator.js";
-import { registerSessionDiffCommand } from "./session-diff.js";
 
 const FIXED_TOOL_DISPLAY_CONFIG: ToolDisplayConfig = {
   ...DEFAULT_TOOL_DISPLAY_CONFIG,
@@ -23,24 +19,6 @@ const FIXED_TOOL_DISPLAY_CONFIG: ToolDisplayConfig = {
 };
 
 export default function toolDisplayExtension(pi: ExtensionAPI): void {
-  // Feature 2: Tool result cards
   registerToolDisplayOverrides(pi, () => FIXED_TOOL_DISPLAY_CONFIG);
-
-  // Feature 1: Thinking labeling
   registerThinkingLabeling(pi);
-
-  // Session tracking
-  registerSessionTracker(pi);
-
-  // Widgets and status indicators
-  registerWidgets(pi);
-
-  // Turn separator
-  registerTurnSeparatorRenderer(pi);
-  pi.on("agent_end", async (_event, ctx) => {
-    injectTurnSeparator(pi, ctx);
-  });
-
-  // Session diff command
-  registerSessionDiffCommand(pi);
 }
